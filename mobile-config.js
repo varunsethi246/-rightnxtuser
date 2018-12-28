131f1e87f212afb242db806c706f746d4f696602
poojagujar3 <poojagujar3@gmail.com>
2018-12-28 18:19:42 +0530

    Karishma added changes.

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,7 @@
 App.info({
   id : "com.rightuser",
-  name: 'Rightnxt User',
-  version: "1.0.0", 
+  name: 'Rightnxt',
+  version: "1.1.0", 
   author: 'iAssure IT',
   email: 'rightnxt123@gmail.com',
   website: 'http://mobileappuser.rightnxt.com'
@@ -18,13 +18,13 @@ App.accessRule('polyblob:*',{type: 'intent'});
 App.accessRule('https://rightnxt.com', { type: 'navigation' });
 App.accessRule('https:*');
 // App.accessRule('https://s3.us-east-2.amazonaws.com/rightnxt1/BannerVideo/n9a6JFjfWRTtDmM9X-original.mp4');
-// App.launchScreens({
-//   'android_mdpi_portrait': 'splashscreen/320.png',
-// });
-// App.icons({
-//   'android_mdpi': 'splashscreen/48.png',
-//   'android_hdpi': 'splashscreen/72.png',
-//   'android_xhdpi': 'splashscreen/96.png',
-//   'android_xxhdpi': 'splashscreen/144.png',
-//   'android_xxxhdpi': 'splashscreen/192.png',
-// });
+App.launchScreens({
+  'android_mdpi_portrait': 'splashscreen/320X320.png',
+});
+App.icons({
+  'android_mdpi': 'splashscreen/48X48.png',
+  'android_hdpi': 'splashscreen/72X72.png',
+  'android_xhdpi': 'splashscreen/96X96.png',
+  'android_xxhdpi': 'splashscreen/144X144.png',
+  'android_xxxhdpi': 'splashscreen/192X192.png',
+});
